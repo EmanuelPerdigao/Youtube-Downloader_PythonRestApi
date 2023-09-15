7c30080b63fb56c3bf130b30d00bd2048df9f648
emanuel perdigao casa <emanuelfsperdigao@gmail.com>
2023-09-15 15:55:26 +0100

    solving bugs in multithreading

--- restApiMain.py
+++ restApiMain.py
@@ -65,4 +65,4 @@ def download_music():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
